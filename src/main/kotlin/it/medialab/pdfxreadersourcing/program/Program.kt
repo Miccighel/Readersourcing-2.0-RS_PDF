80fb8f3592d426025585f91abebd729176b54411
Miccighel <miccighel@gmail.com>
2018-07-06 16:09:46 +0200

    Maven integration wired up (2). Build configurations prepared.

---
+++ src/main/kotlin/it/medialab/pdfxreadersourcing/program/Program.kt
@@ -0,0 +1,14 @@
+package it.medialab.pdfxreadersourcing.program
+
+object Program {
+
+    @JvmStatic
+    fun main(arguments: Array<String>) {
+
+        println("Hello World")
+
+    }
+
+}
+
+
---
+++ src/test/kotlin/it/medialab/pdfxreadersourcing/program/ProgramTest.kt
@@ -0,0 +1,22 @@
+package it.medialab.pdfxreadersourcing.program
+
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class ProgramTest {
+
+    @Test
+    @DisplayName("Main")
+
+    fun testMain() {
+
+        println("[ProgramTest main] - Test begins.")
+
+        assertEquals(true, true)
+
+        println("[ProgramTest main] - Test ends.")
+
+    }
+
+}
