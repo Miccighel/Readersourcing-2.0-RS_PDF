c326c3c40ca74fa1a9120f1bf33ee3eb0d478553
Michael Soprano <miccighel@gmail.com>
2018-08-10 18:07:19 +0200

    Renaming operation on variable

--- src/main/kotlin/it/medialab/pdfxreadersourcing/publications/PublicationController.kt
+++ src/main/kotlin/it/medialab/pdfxreadersourcing/publications/PublicationController.kt
@@ -11,7 +11,7 @@ class PublicationController {
 
     private var models = mutableListOf<PublicationModel>()
     private lateinit var inputPath: String
-    private lateinit var ouputPath: String
+    private lateinit var outputPath: String
     private var numberOfPublications: Int = 0
     private var logger = LogManager.getLogger()
 
@@ -22,29 +22,29 @@ class PublicationController {
     fun load(publicationInputPath: String, publicationOutputPath: String) {
 
         this.inputPath = publicationInputPath
-        this.ouputPath = publicationOutputPath
+        this.outputPath = publicationOutputPath
 
         logger.info("Data set loading started.")
         logger.info("Path: \"${this.inputPath}\".")
 
-        val outputDirectory = File(this.ouputPath)
+        val outputDirectory = File(this.outputPath)
 
-        if (this.ouputPath == Constants.OUTPUT_PATH) {
+        if (this.outputPath == Constants.OUTPUT_PATH) {
             logger.info("Checking if ${Constants.PROGRAM_NAME} output dir. exists.")
             if (!outputDirectory.exists()) {
                 logger.info("Output dir. not exists.")
                 if (outputDirectory.mkdirs()) {
                     logger.info("Output dir. created.")
-                    logger.info("Path: \"${this.ouputPath}\".")
+                    logger.info("Path: \"${this.outputPath}\".")
                 }
             } else {
                 logger.info("Output dir. already exists.")
                 logger.info("Output dir. creation skipped.")
-                logger.info("Path: \"${this.ouputPath}\".")
+                logger.info("Path: \"${this.outputPath}\".")
             }
         } else {
             logger.info("Custom output dir. presence already checked.")
-            logger.info("Path: \"${this.ouputPath}\".")
+            logger.info("Path: \"${this.outputPath}\".")
         }
         try {
             File(this.inputPath).walkTopDown().forEach {
@@ -57,10 +57,10 @@ class PublicationController {
                 if (!it.isDirectory) {
                     if (Files.probeContentType(it.toPath()) == "application/pdf") {
                         val model = PublicationModel()
-                        if (this.ouputPath == Constants.OUTPUT_PATH)
+                        if (this.outputPath == Constants.OUTPUT_PATH)
                             model.loadData(it)
                         else
-                            model.loadData(it, this.ouputPath)
+                            model.loadData(it, this.outputPath)
                         models.plusAssign(model)
                     }
                 }
